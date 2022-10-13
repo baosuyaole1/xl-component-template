fd411c1f3bf42fd5c7510438dda26ae638679d77
baosuyaole <bsylserverit@163.com>
2022-10-13 13:32:41 +0800

    chore: 修改格式化配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -17,5 +17,5 @@ module.exports = {
   htmlWhitespaceSensitivity: 'ignore', // 对HTML全局空白不敏感
   vueIndentScriptAndStyle: false, // 不对vue中的script及style标签缩进
   endOfLine: 'lf', // 结束行形式
-  embeddedLanguageFormatting: 'auto' // 对引用代码进行格式化
-}
+  embeddedLanguageFormatting: 'auto', // 对引用代码进行格式化
+};
